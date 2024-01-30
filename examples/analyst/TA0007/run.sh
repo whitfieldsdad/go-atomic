a6ffd842566ee879d17302fa01471916be605ec8
Tyler Fisher <tylerfisher@tylerfisher.ca>
2024-01-30 11:26:58 -0500

    Add example of running all tasks related to TA0007: Discovery

---
+++ examples/analyst/TA0007/run.sh
@@ -0,0 +1,1 @@
+go run main.go tasks --atomics-path=~/src/atomic-red-team/atomics/ --attack-tactics-to-techniques-path=data/attack-tactics-to-techniques.json exec --attack-tactic-id TA0007 --auto > TA0007.jsonl
